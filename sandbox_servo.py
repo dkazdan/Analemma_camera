3ba2b969e8af5ae0c9598dd8e2cb25b8d000cf91
dkazdan <30479240+dkazdan@users.noreply.github.com>
2024-02-14 15:42:41 +0000

    Add files via upload
    Got timing right for MG 996 R servos, which have 120 degree travel.

--- sandbox_servo.py
+++ sandbox_servo.py
@@ -1,12 +1,15 @@
 """
 Practice controlling model servos
 Several tutorials out there.
+https://www.instructables.com/Controlling-Servo-Motor-Sg90-With-Raspberry-Pi-4/
 Involved one with graphic slider interfaces: https://www.circuitbasics.com/how-to-use-servos-on-the-raspberry-pi/
 
 """
 
 import RPi.GPIO as GPIO
 from time import sleep
+# stop warnings
+GPIO.setwarnings(False)
 
 # set up the GPIO mode as BOARD to reference the PINs and not the BCM pins
 GPIO.setmode(GPIO.BOARD)
@@ -18,25 +21,40 @@ GPIO.setup(11, GPIO.OUT) # GPIO17, pin 11
 # GPIO.setup(servoPIN, GPIO.OUT)
 
 pwm=GPIO.PWM(11, 50) # fifty pulses/second on that pin
+print('set zero angle')
 pwm.start(0)         # starting at duty cycle zero
+sleep(2)
 
-# Left -90 deg is 5%, Neutral is 7.5%, and Right +90 deg is 10%
-pwm.ChangeDutyCycle(5) # left -90 deg position
-sleep(1)
-pwm.ChangeDutyCycle(7.5) # neutral position
-sleep(1)
-pwm.ChangeDutyCycle(10) # right +90 deg position
-sleep(1)
+def test_servo():
+    # Left -60 deg is 2%, Neutral is 6%, and Right +60 deg is 10%
+    print('left')
+    pwm.ChangeDutyCycle(2) # left -90 deg position
+    sleep(1)
+    print('centered')
+    pwm.ChangeDutyCycle(6) # neutral position
+    sleep(1)
+    print('right')
+    pwm.ChangeDutyCycle(10) # right +90 deg position
+    sleep(1)
 
-# clean up by stopping the PWM signal and running the cleanup function
-pwm.stop()
-GPIO.cleanup()
+    # clean up by stopping the PWM signal and running the cleanup function
+    pwm.stop()
+    GPIO.cleanup()
 
-# Might be useful:
+# Might be useful: TODO: Change this to match above numbers.
 def setAngle(angle):
-    duty = angle / 18 + 2
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(11, GPIO.OUT) # GPIO17, pin 11
+    duty = (angle / 18) + 2
     GPIO.output(11, True) # sdame as .start, .stop?
     pwm.ChangeDutyCycle(duty)
     sleep(1)
     GPIO.output(11, False)
-    pwm.ChangeDutyCycle(duty)
+    pwm.ChangeDutyCycle(0)
+    
+if __name__ == '__main__':
+    test_servo()
+    sleep(1)
+    print('set to 45 degrees')
+    setAngle(45)
+    print('\nend')
